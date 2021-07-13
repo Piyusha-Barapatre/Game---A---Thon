80b8277c7733b9c2d2c4f31ac22b71751b7c15b0
Piyusha-Barapatre <85167734+Piyusha-Barapatre@users.noreply.github.com>
2021-07-13 19:28:59 +0530

    Create problem_02.c

---
+++ problem_02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int A, B, C;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &A, &B); 
+  
+    product = A * B;
+  
+    printf("C = %d", product);
+  
+    return 0;
+}
